cdf15c0ebf019246d2bbdb01281ea6f9c72a0a24
Dani Santamaría <santaka87@gmail.com>
2023-11-04 08:26:22 +0100

    use php parser to generate the stub factory

--- src/Generator/StubGenerator.php
+++ src/Generator/StubGenerator.php
@@ -2,6 +2,21 @@
 
 namespace Santakadev\AnyObject\Generator;
 
+use PhpParser\BuilderFactory;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\Instanceof_;
+use PhpParser\Node\Expr\Match_;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\MatchArm;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\If_;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\PrettyPrinter\Standard;
 use ReflectionClass;
 use Santakadev\AnyObject\Parser\Parser;
 
@@ -20,39 +35,52 @@ class StubGenerator
 
         $reflectionClass = new ReflectionClass($root->type->class);
         $name = $reflectionClass->getShortName();
-        $namespace = $reflectionClass->getNamespaceName();
+        $classNamespace = $reflectionClass->getNamespaceName();
         $stubName = $name . 'Factory';
-        $currentNamespace = __NAMESPACE__;
+        $factoryNamespace = 'Santakadev\\AnyObject\\Tests\\Generator\\Generated';
 
-        $file = "<?php
+        $factory = new BuilderFactory;
+        $node = $factory->namespace($factoryNamespace)
+            ->addStmt($factory->use('Faker\Factory'))
+            ->addStmt($factory->use("$classNamespace\\$name"))
+            ->addStmt($factory->class($stubName)
+                ->makeFinal()
 
-namespace $currentNamespace\Tests\Generated;
+                ->addStmt($factory->method('with')
+                    ->makePublic()
+                    ->setReturnType($name)
+                    ->addParam(
+                        $factory
+                            ->param('value')
+                            ->setType('string|ValueNotProvided')
+                            ->setDefault($factory->new(new Name('ValueNotProvided')))
+                    )
+                    ->addStmt(new If_(new Instanceof_(new Variable('value'), new Name('ValueNotProvided')), [
+                        'stmts' => [
+                            new Expression(new Assign(new Variable('faker'), $factory->staticCall(new Name('Factory'), 'create'))),
+                            new Expression(new Assign(new Variable('value'), $factory->methodCall(new Variable('faker'), 'text' )))
+                        ]
+                    ]))
+                    ->addStmt(new Return_($factory->new($name, [new Variable('value')])))
+                )
 
-use Faker\Factory;
-use $namespace\\$name;
+                ->addStmt($factory->method('build')
+                    ->setReturnType($name)
+                    ->makePublic()
+                    ->addStmt(new Return_(new MethodCall(new Variable('this'), 'with')))
+                )
+            )
 
-final class $stubName
-{
-    public function with(string|ValueNotProvided \$value = new ValueNotProvided): $name
-    {
-        if (\$value instanceof ValueNotProvided) {
-            \$value = (Factory::create())->text();
+            ->getNode();
+        $stmts = [$node];
+        $prettyPrinter = new Standard();
+        $file = $prettyPrinter->prettyPrintFile($stmts) . "\n";
+
+        if (!is_dir(__DIR__ . "/../../tests/Generator/Generated")) {
+            mkdir(__DIR__ . "/../../tests/Generator/Generated");
         }
 
-        return new StringObject(\$value);
-    }
-
-    public function build(): $name
-    {
-        return self::with();
-    }
-}
-";
-        if (!is_dir(__DIR__ . "/../tests/Generated/")) {
-            mkdir(__DIR__ . "/../tests/Generated/");
-        }
-
-        file_put_contents(__DIR__ . "/../tests/Generated/$stubName.php", $file);
+        file_put_contents(__DIR__ . "/../../tests/Generator/Generated/$stubName.php", $file);
 
         $this->generateAnyFile();
         $this->generateValueNotProvidedFile();
@@ -62,37 +90,44 @@ final class $stubName
 
     public function generateAnyFile(): void
     {
-        $any = '<?php
+        $factory = new BuilderFactory;
+        $node = $factory->namespace('Santakadev\AnyObject\Tests\Generator\Generated')
+            ->addStmt($factory->use('Santakadev\AnyObject\Tests\TestData\ScalarTypes\StringObject'))
+            ->addStmt($factory->class('Any')
+                ->makeFinal()
 
-namespace Santakadev\AnyObject\Tests\Generated;
-
-use Santakadev\AnyObject\Tests\Generator\Generated\StringObjectFactory;use Santakadev\AnyObject\Tests\TestData\ScalarTypes\StringObject;
-
-class Any
-{
-    public static function of(string $class): StringObjectFactory
-    {
-        return match ($class) {
-            StringObject::class => new StringObjectFactory()
-        };
-    }
-}
-';
-
-        file_put_contents(__DIR__ . "/../tests/Generated/Any.php", $any);
+                ->addStmt($factory->method('of')
+                    ->makePublic()
+                    ->makeStatic()
+                    ->setReturnType('StringObjectFactory')
+                    ->addParam(
+                        $factory
+                            ->param('class')
+                            ->setType('string')
+                    )
+                    ->addStmt(new Return_(new Match_(new Variable('class'), [
+                        new MatchArm([new ClassConstFetch(new Name('StringObject'), 'class')], $factory->new(new Name('StringObjectFactory')))
+                    ])))
+                )
+            )
+            ->getNode();
+        $stmts = [$node];
+        $prettyPrinter = new Standard();
+        $any = $prettyPrinter->prettyPrintFile($stmts) . "\n";
+        file_put_contents(__DIR__ . "/../../tests/Generator/Generated/Any.php", $any);
     }
 
     public function generateValueNotProvidedFile(): void
     {
-        $any = '<?php
-
-namespace Santakadev\AnyObject\Tests\Generated;
-
-class ValueNotProvided
-{
-}
-';
-
-        file_put_contents(__DIR__ . "/../tests/Generated/ValueNotProvided.php", $any);
+        $factory = new BuilderFactory;
+        $node = $factory->namespace('Santakadev\AnyObject\Tests\Generator\Generated')
+            ->addStmt($factory->class('ValueNotProvided')
+                ->makeFinal()
+            )
+            ->getNode();
+        $stmts = [$node];
+        $prettyPrinter = new Standard();
+        $valueNotProvided = $prettyPrinter->prettyPrintFile($stmts) . "\n";
+        file_put_contents(__DIR__ . "/../../tests/Generator/Generated/ValueNotProvided.php", $valueNotProvided);
     }
 }
