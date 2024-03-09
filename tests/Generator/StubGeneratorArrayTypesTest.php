eb68b0845933aabafbeb18c394ba17c043920189
Dani Santamaría <santaka87@gmail.com>
2024-03-09 07:00:36 +0100

    generator: add tests for the rest of scalar arrays

--- tests/Generator/StubGeneratorArrayTypesTest.php
+++ tests/Generator/StubGeneratorArrayTypesTest.php
@@ -5,8 +5,12 @@ namespace Santakadev\AnyObject\Tests\Generator;
 use ApprovalTests\Approvals;
 use Santakadev\AnyObject\Generator\StubGenerator;
 use Santakadev\AnyObject\Tests\AnyObjectTestCase;
+use Santakadev\AnyObject\Tests\Generator\Generated\AnyGenericArrayOfBoolObject;
+use Santakadev\AnyObject\Tests\Generator\Generated\AnyGenericArrayOfFloatObject;
 use Santakadev\AnyObject\Tests\Generator\Generated\AnyGenericArrayOfIntObject;
 use Santakadev\AnyObject\Tests\Generator\Generated\AnyGenericArrayOfStringObject;
+use Santakadev\AnyObject\Tests\TestData\ArrayTypes\GenericArrayOfBoolObject;
+use Santakadev\AnyObject\Tests\TestData\ArrayTypes\GenericArrayOfFloatObject;
 use Santakadev\AnyObject\Tests\TestData\ArrayTypes\GenericArrayOfIntObject;
 use Santakadev\AnyObject\Tests\TestData\ArrayTypes\GenericArrayOfStringObject;
 
@@ -43,6 +47,34 @@ class StubGeneratorArrayTypesTest extends AnyObjectTestCase
         $this->assertLessThanOrEqual(50, count($object->value));
         foreach ($object->value as $item) {
             $this->assertIsInt($item);
+        }
+    }
+
+    public function test_generator_array_of_float(): void
+    {
+        $generator = new StubGenerator();
+        $text = $generator->generate(GenericArrayOfFloatObject::class);
+        Approvals::verifyString($text);
+        $object = AnyGenericArrayOfFloatObject::build();
+        $this->assertIsArray($object->value);
+        $this->assertGreaterThanOrEqual(0, count($object->value));
+        $this->assertLessThanOrEqual(50, count($object->value));
+        foreach ($object->value as $item) {
+            $this->assertIsFloat($item);
+        }
+    }
+
+    public function test_generator_array_of_bool(): void
+    {
+        $generator = new StubGenerator();
+        $text = $generator->generate(GenericArrayOfBoolObject::class);
+        Approvals::verifyString($text);
+        $object = AnyGenericArrayOfBoolObject::build();
+        $this->assertIsArray($object->value);
+        $this->assertGreaterThanOrEqual(0, count($object->value));
+        $this->assertLessThanOrEqual(50, count($object->value));
+        foreach ($object->value as $item) {
+            $this->assertIsBool($item);
         }
     }
 }
