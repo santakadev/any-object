7524e8ce8df102a3c5f7b6db56e6d59c6d763972
Dani Santamaría <santaka87@gmail.com>
2024-11-01 15:09:11 +0100

    remove test

--- tests/CustomizeRandomTest.php
+++ tests/CustomizeRandomTest.php
@@ -27,26 +27,6 @@ class CustomizeRandomTest extends AnyObjectTestCase
     }
 
     /** @dataProvider anyProvider */
-    public function test_random_digit(AnyObject $any): void
-    {
-        $this->assertAll(
-            fn () => $any->of(RandomDigitCustomizedObject::class)->value,
-            [
-                fn ($value) => $value === 0,
-                fn ($value) => $value === 1,
-                fn ($value) => $value === 2,
-                fn ($value) => $value === 3,
-                fn ($value) => $value === 4,
-                fn ($value) => $value === 5,
-                fn ($value) => $value === 6,
-                fn ($value) => $value === 7,
-                fn ($value) => $value === 8,
-                fn ($value) => $value === 9,
-            ]
-        );
-    }
-
-    /** @dataProvider anyProvider */
     public function test_random_uuid(AnyObject $any): void
     {
         $value = $any->of(UuidCustomizedObject::class)->value;
--- tests/Generator/FactoryGeneratorCustomizedTypesTest.php
+++ tests/Generator/FactoryGeneratorCustomizedTypesTest.php
@@ -27,27 +27,4 @@ class FactoryGeneratorCustomizedTypesTest extends FactoryGeneratorTestCase
             ]
         );
     }
-
-    public function test_customized_int_with_random_digit(): void
-    {
-        $this->generateFactoryFor(RandomDigitCustomizedObject::class);
-
-        $text = $this->readGeneratedAnyFileFor(RandomDigitCustomizedObject::class);
-        Approvals::verifyString($text);
-        $this->assertAll(
-            fn () => AnyRandomDigitCustomizedObject::build()->value,
-            [
-                fn ($value) => $value === 0,
-                fn ($value) => $value === 1,
-                fn ($value) => $value === 2,
-                fn ($value) => $value === 3,
-                fn ($value) => $value === 4,
-                fn ($value) => $value === 5,
-                fn ($value) => $value === 6,
-                fn ($value) => $value === 7,
-                fn ($value) => $value === 8,
-                fn ($value) => $value === 9,
-            ]
-        );
-    }
 }
